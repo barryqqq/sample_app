fc2e986c6b2e8524063e45145f3c4bf99e3e5ba1
Nick <iamcooc@gmail.com>
2014-03-09 23:48:23 -0400

    add some test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
+
   end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save 
+  		#handle successful save
+  	else
+  		render 'new'
+  	end		
+  end	
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,14 +1,40 @@
 require 'spec_helper'
 
 describe "UserPages" do
-
+	
   subject {page}
   describe "signup page" do
   	before { visit signup_path }	
+  	let(:submit) { "Create my account" }
 
-  	it {should have_content('Sign Up')}
+  	describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+  	describe "with valid information" do
+
+
+  		before do
+			fill_in "user_name" with: "Example User"
+			fill_in "user_email" with: "example_u@g.com"
+			fill_in "password" with: "qazwsxedc"
+			fill_in "confirm_password" with: "qazwsxedc"
+
+			click_button "Create My Account"
+		end
+		it "should create a user" do
+        	expect { click_button submit }.to change(User, :count).by(1)
+      	end
+  	end	
+
   end
-  
+
+	
+	
+
+
 =begin	
   describe "GET /user_pages" do
     it "works! (now write some real specs)" do
