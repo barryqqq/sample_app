55cb324136203dc7435797e88a425dd0c45f8349
iamcooc <iamcooc@gmail.com>
2014-03-28 17:10:58 -0400

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ SampleApp::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+ 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -32,8 +32,8 @@ SampleApp::Application.configure do
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['around_you_and_me'],
-      :access_key_id => ENV['AKIAISIXGQLGCTCLR3DQ'],
-      :secret_access_key => ENV['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']  
+      :access_key_id => ENV[''],
+      :secret_access_key => ENV['']  
 
     }
 
